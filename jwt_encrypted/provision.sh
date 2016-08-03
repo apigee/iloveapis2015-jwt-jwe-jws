4c00ff18cc3456554640ceb75b7fca66faaa9f3b
Dino Chiesa <dchiesa@apigee.com>
2016-08-03 12:59:27 -0700

    updating provision.sh script for jwt_encrypted to accommodate a 201 RC

--- jwt_encrypted/provision.sh
+++ jwt_encrypted/provision.sh
@@ -16,7 +16,7 @@
 # 
 # 
 # Created: <Thu Nov  5 13:30:29 2015>
-# Last Updated: <2016-July-28 08:44:43>
+# Last Updated: <2016-August-03 12:58:54>
 #
 
 apiname=jwt_encrypted
@@ -698,7 +698,7 @@ if [ $resetonly -eq 0 ] ; then
     MYCURL -X POST -H content-type:application/json \
       $mgmtserver/v1/o/$org/e/$env/vaults \
       -d '{ "name": "'$vaultname'" }'
-    if [ ${CURL_RC} -ne 200 ]; then
+    if [[ ${CURL_RC} -ne 200 && ${CURL_RC} -ne 201 ]]; then
       echo failed.
       cat ${CURL_OUT} 
       echo
